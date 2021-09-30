a30ba0e7f2ebf31f4914a127e62c1aba3cfe2f0a
Barta Balázs <barta.balazs00@gmail.com>
2021-09-30 11:19:44 +0200

    Veletlen osztály fájljának beolvasása

--- src/hu/bartabalazs/Veletlen.java
+++ src/hu/bartabalazs/Veletlen.java
@@ -1,4 +1,27 @@
 package hu.bartabalazs;
 
-public class Veletlen {
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public final class Veletlen {
+    private static Random random = new Random();
+
+    private Veletlen() {}
+
+    private List<String> feltolt(String fajlnev){
+        List<String> fajlSorai = new ArrayList<>();
+        try {
+            fajlSorai = Files.readAllLines(Paths.get(fajlnev));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return fajlSorai;
+    }
 }
